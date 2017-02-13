0ea7774d432517915426224e7b8d5d67efc48162
geng <gengyabc@aliyun.com>
2017-02-13 08:42:46 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+  public static void main(String[] args) {
+    System.out.println("初生婴儿哇哇大哭");
+    int age = (int)(Math.random() * 100);
+    System.out.println(age);
+  }
+}
